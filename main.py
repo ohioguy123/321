8e73f9ffe05888c550e7eaca05dd9ab6d9aedc3e
ohioguy123 <119048178+ohioguy123@users.noreply.github.com>
2023-11-15 21:29:10 +0300

    main.py

--- main.py
+++ main.py
@@ -24,8 +24,7 @@ def display_menu():
     """)
 
 def install_busware():
-    os.system("git clone https://github.com/ohioguy123/BusWare.git")
-    os.system("cd BusWare && pip3 install -r requirements.txt && python3 BusWare.py")
+    os.system("git clone https://github.com/ohioguy123/BusWare.git && cd BusWare && pip3 install -r requirements.txt && python3 BusWare.py")
     clear_screen()
     print("""
     BusWare successfully installed and executed!
